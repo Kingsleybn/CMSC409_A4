d313152aae2d8957437509cce3bee5d18bee87d8
tiany4 <tiany4@vcu.edu>
2017-12-02 21:03:23 -0500

    TDM output

--- src/CMSC409_A4.java
+++ src/CMSC409_A4.java
@@ -1,12 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class CMSC409_A4 {
     public static void main(String[] args) throws FileNotFoundException {
-        //Get from file instead
-        //String sentences = "The autonomous sedan will be able to travel sedan on any type of road at speeds of up to 60 miles per hour. \b On future of machine learning, sedan Ray Kurzweil has predicted that we are only 28 years away from the Singularity or when self-improving artificial super-intelligence will far exceed human intelligence.\b This gets the car from 0 to 60 miles per hour (that is, to 97 kilometers per hour) in 3.2 seconds.";
 
         Scanner scan = new Scanner(new File("sentences.txt"));
         ArrayList<String> temp = new ArrayList<>();
@@ -38,8 +37,13 @@ public class CMSC409_A4 {
             sb.delete(0, sb.length());
         }
 
-        //Build and print TDM
+
+        //Build and print TDM, outputs to TDM.csv
         TDMer tdm = new TDMer(tokens);
-        tdm.printTDM();
+        try {
+            tdm.printTDM();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/TDMer.java
+++ src/TDMer.java
@@ -1,81 +1,81 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
-public class TDMer
-{
+public class TDMer {
     private String[] sentences;
     private ArrayList<int[]> TDM;
     private ArrayList<String> words;
     private int cols;
 
-    public TDMer(String[] input)
-    {
-        this.sentences=input;
-        this.cols=input.length;
-        this.words= new ArrayList<>();
+    public TDMer(String[] input) {
+        this.sentences = input;
+        this.cols = input.length;
+        this.words = new ArrayList<>();
         this.TDM = this.buildTDM();
     }
 
-    private ArrayList<int[]> buildTDM()
-    {
+    private ArrayList<int[]> buildTDM() {
         String[] tempWords;
         int index;
 
         ArrayList<int[]> tempTDM = new ArrayList<>();
 
         //Repeat for each sentence
-        for(int i=0; i<this.cols; i++)
-        {
-            tempWords=this.sentences[i].split(" ");
+        for (int i = 0; i < this.cols; i++) {
+            tempWords = this.sentences[i].split(" ");
 
             //Repeat for each word in sentence
-            for(int j=0; j<tempWords.length;j++)
-            {
+            for (int j = 0; j < tempWords.length; j++) {
                 //Ignore spaces
-                if(!tempWords[j].equals(" ") && !tempWords[j].equals(""))
-                {
+                if (!tempWords[j].equals(" ") && !tempWords[j].equals("")) {
                     //See if word has been used before
-                    if(this.words.contains(tempWords[j]))
-                    {
+                    if (this.words.contains(tempWords[j])) {
                         //Look up word in word list and increment TDM
-                        index=this.words.indexOf(tempWords[j]);
+                        index = this.words.indexOf(tempWords[j]);
                         tempTDM.get(index)[i]++;
 
                     }
                     //Add word to TDM if needed
-                    else
-                    {
+                    else {
                         //Add word to word list
                         this.words.add(tempWords[j]);
 
                         //Add a new column to TDM and increment
                         int[] test = new int[this.cols];
-                        for(int g=0; g<this.cols; g++){ test[g]=0; }
+                        for (int g = 0; g < this.cols; g++) {
+                            test[g] = 0;
+                        }
                         tempTDM.add(test);
 
-                        index=this.words.indexOf(tempWords[j]);
+                        index = this.words.indexOf(tempWords[j]);
                         tempTDM.get(index)[i]++;
                     }
                 }
             }
         }
-
         return tempTDM;
     }
 
     //Print word list and TDM
-    void printTDM()
-    {
-        for(int i=0; i<this.words.size(); i++)
-        {
-            System.out.print(padRight(this.words.get(i),15));
+    void printTDM() throws IOException {
+        File file = new File("TDM.csv");
 
-            for (int j=0;j<this.cols;j++)
-            {
-                System.out.print(this.TDM.get(i)[j] + "\t");
+        FileWriter fw = new FileWriter(file);
+
+        for (int i = 0; i < this.words.size(); i++) {
+            System.out.print(padRight(this.words.get(i), 15));
+            fw.write(this.words.get(i) + ",");
+
+            for (int j = 0; j < this.cols; j++) {
+                System.out.print(this.TDM.get(i)[j] + " ");
+                fw.write(this.TDM.get(i)[j] + ",");
             }
-
             System.out.println();
+            fw.write("\n");
         }
+        fw.close();
     }
 
     public static String padRight(String s, int n) {
